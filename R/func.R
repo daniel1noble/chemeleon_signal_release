42b6ebd9ccecb84904a530fcdbd43ac49a244df6
Daniel Noble <daniel.wa.noble@gmail.com>
2021-05-06 20:06:34 +1000

    Updated supp to fix dataframe for table output

--- R/func.R
+++ R/func.R
@@ -5,3 +5,33 @@ p_value <- function(x){
   if(x >= 0.01) {tmp = round(x, digits =2)}
   return(tmp)
 }
+
+
+wald_table <- function(model_main, model_inter){
+  
+       inter <- Wald_test(model_inter, 
+                                        constraints = constrain_zero(7:9),
+                                        vcov="CR2", tidy = TRUE)
+  BodyRegion <- Wald_test(model_main, 
+                                             constraints = constrain_zero(4:6),
+                                             vcov="CR2", tidy = TRUE)
+  SVL <- Wald_test(model_main, 
+                                      constraints = constrain_zero(2),
+                                      vcov="CR2", tidy = TRUE)
+  Pop <- Wald_test(model_main, 
+                                      constraints = constrain_zero(3),
+                                      vcov="CR2", tidy = TRUE)
+  
+  WaldTab <- rbind(SVL,
+                                      Pop, 
+                                      BodyRegion, 
+                                      inter)
+  WaldTab <- WaldTab %>% 
+    mutate(Variable = c("SVL", 
+                        "Population", 
+                        "Body Region", 
+                        "Population:Body Region")) %>% 
+    select(Variable, Fstat, df_num, df_denom, p_val) %>% as.data.frame()
+  
+  return(WaldTab)
+}
